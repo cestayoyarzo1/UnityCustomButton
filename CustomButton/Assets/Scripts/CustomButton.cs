747aa93477661cf8ad30d5e7187277c197da64a5
Carlos Estay <39070811+cestayoyarzo1@users.noreply.github.com>
2020-02-16 20:37:38 -0700

    CUstomized button Addid other events handling
    -  OnPointerEnter
    -  OnPointerExit
    -  OnPointerDown
    -  OnPointerUp
    -  OnPointerDown

---
+++ CustomButton/Assets/Scripts/CustomButton.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CustomButton : Button
+{
+
+    public UnityEvent onPointerEnter { get; set; }
+    public UnityEvent onPointerExit { get; set; }
+    public UnityEvent onPointerDown { get; set; }
+    public UnityEvent onPointerUp { get; set; }
+    public UnityEvent onPointerClick { get; set; }
+
+    protected override void Awake()
+    {
+        onPointerEnter = new UnityEvent();
+        onPointerExit = new UnityEvent();
+        onPointerDown = new UnityEvent();
+        onPointerUp = new UnityEvent();
+        onPointerClick = new UnityEvent();
+    }
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        base.OnPointerEnter(eventData);
+        onPointerEnter.Invoke();
+    }
+
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        base.OnPointerExit(eventData);
+        onPointerExit.Invoke();
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        base.OnPointerDown(eventData);
+        onPointerDown.Invoke();
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        base.OnPointerDown(eventData);
+        onPointerUp.Invoke();
+    }
+
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        base.OnPointerClick(eventData);
+        onPointerClick.Invoke();
+    }
+
+}
---
+++ CustomButton/Assets/Scripts/TestButton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TestButton : MonoBehaviour
+{
+    CustomButton testButton;
+
+    Color currentColor, hoverColor;
+
+    void Start()
+    {
+        testButton = GetComponentInChildren<CustomButton>();
+        testButton.onPointerClick.AddListener(ButtonClicked);
+        testButton.onPointerEnter.AddListener(PointerEnter);
+        testButton.onPointerExit.AddListener(PointerExit);
+        testButton.onPointerUp.AddListener(PointerUp);
+        testButton.onPointerDown.AddListener(PointerDown);
+        hoverColor = Color.blue;
+        currentColor = testButton.image.color;
+    }
+
+    private void PointerUp()
+    {
+        print("Pointer is Up");
+    }
+
+    private void PointerDown()
+    {
+        print("Pointer is Down");
+    }
+
+    private void PointerExit()
+    {
+        print("Pointer has exited");
+        testButton.image.color = currentColor;
+    }
+
+    private void PointerEnter()
+    {
+        print("Pointer has entered");
+        testButton.image.color = hoverColor;
+    }
+
+    private void ButtonClicked()
+    {
+        print("Button has been clicked");
+    }
+
+    void Update()
+    {
+        
+    }
+}
